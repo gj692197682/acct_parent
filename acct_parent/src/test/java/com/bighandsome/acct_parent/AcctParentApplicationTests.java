6c09206d801cebb07d936486784098a8bb552a6c
gj692197682 <1241569738@qq.com>
2021-01-31 21:24:16 +0800

    初始化项目

---
+++ acct_parent/src/test/java/com/bighandsome/acct_parent/AcctParentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bighandsome.acct_parent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcctParentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
